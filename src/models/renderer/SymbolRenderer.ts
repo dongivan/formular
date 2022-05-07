34288f03864416702597e3c4de551412165527fd
Yifan Dong <588153@qq.com>
2022-05-07 08:45:12 +0800

    fix: fix a type bug

--- src/models/renderer/SymbolRenderer.ts
+++ src/models/renderer/SymbolRenderer.ts
@@ -1,5 +1,5 @@
 import Formula from "../Formula";
-import { MathChar } from "../math-char";
+import { MathChar, OperandChar } from "../math-char";
 import { MathSymbol, OperandSymbol, OperatorSymbol } from "../math-symbol";
 
 export default abstract class BaseSymbolRenderer<R> {
@@ -17,11 +17,11 @@ export default abstract class BaseSymbolRenderer<R> {
     if (symbol instanceof OperatorSymbol) {
       return this.renderOperator(symbol, leftOperand, rightOperand);
     } else {
-      return this.renderOperand(symbol);
+      return this.renderOperand(symbol as OperandSymbol<OperandChar>);
     }
   }
 
-  abstract renderOperand(symbol: OperandSymbol<MathChar>): R;
+  abstract renderOperand(symbol: OperandSymbol<OperandChar>): R;
 
   abstract renderOperator(
     symbol: OperatorSymbol,
