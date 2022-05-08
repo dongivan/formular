46a3d4453b7266616677365181c711ee95f5da23
Yifan Dong <588153@qq.com>
2022-05-08 23:10:18 +0800

    feat: SvgIcon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg class="svg-icon">
+    <use :xlink:href="iconNameRef"></use>
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+const props = defineProps({
+  name: { type: String, required: true },
+});
+
+const iconNameRef = computed(() => {
+  return `#svg-icon-${props.name}`;
+});
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
+
+const formularSvgsPath = path.join(__dirname, "src/assets/formular-svgs");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.module.rule("svg").exclude.add(formularSvgsPath).end();
+    config.module
+      .rule("svg-sprite-loader")
+      .test(/\.svg$/)
+      .include.add(formularSvgsPath)
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "svg-icon-[folder]-[name]",
+      });
+  },
 });
