4815483a71f860a50d6febd4df426e0d5378f785
Yifan Dong <588153@qq.com>
2022-05-10 09:17:48 +0800

    feat: greek letters

--- src/models/renderer/mathjax-latex/CharRenderer.ts
+++ src/models/renderer/mathjax-latex/CharRenderer.ts
@@ -1,8 +1,63 @@
-import { Cursor, MathChar, Placeholder } from "../../math-char";
+import { Cursor, MathChar, Placeholder, Variable } from "../../math-char";
 import { findByClass, replace } from "../../utils";
 import CharRenderer from "../CharRenderer";
 
 const parenPrefixes = ["", "\\big", "\\Big", "\\bigg", "\\Bigg"];
+
+const greekLetters: Record<string, string> = {
+  "lower-alpha": "\\alpha",
+  "upper-alpha": "A",
+  "lower-beta": "\\beta",
+  "upper-beta": "B",
+  "lower-gamma": "\\gamma",
+  "upper-gamma": "\\Gamma",
+  "lower-delta": "\\delta",
+  "upper-delta": "\\Delta",
+  "lower-epsilon": "\\epsilon",
+  "var-epsilon": "\\varepsilon",
+  "upper-epsilon": "E",
+  "lower-zeta": "\\zeta",
+  "upper-zeta": "Z",
+  "lower-eta": "\\eta",
+  "upper-eta": "H",
+  "lower-theta": "\\theta",
+  "var-theta": "\\vartheta",
+  "upper-theta": "\\Theta",
+  "lower-iota": "\\iota",
+  "upper-iota": "I",
+  "lower-kappa": "\\kappa",
+  "upper-kappa": "K",
+  "lower-lambda": "\\lambda",
+  "upper-lambda": "\\Lambda",
+  "lower-mu": "\\mu",
+  "upper-mu": "M",
+  "lower-nu": "\\nu",
+  "upper-nu": "N",
+  "lower-xi": "\\xi",
+  "upper-xi": "\\Xi",
+  "lower-o": "o",
+  "upper-o": "O",
+  "lower-pi": "\\pi",
+  "upper-pi": "\\Pi",
+  "lower-rho": "\\rho",
+  "var-rho": "\\varrho",
+  "upper-rho": "P",
+  "lower-sigma": "\\sigma",
+  "upper-sigma": "\\Sigma",
+  "lower-tau": "\\tau",
+  "upper-tau": "T",
+  "lower-upsilon": "\\upsilon",
+  "upper-upsilon": "\\Upsilon",
+  "lower-phi": "\\phi",
+  "var-phi": "\\varphi",
+  "upper-phi": "\\Phi",
+  "lower-chi": "\\chi",
+  "upper-chi": "X",
+  "lower-psi": "\\psi",
+  "upper-psi": "\\Psi",
+  "lower-omega": "\\omega",
+  "upper-omega": "\\Omega",
+};
 
 export default class LatexCharRenderer extends CharRenderer<string> {
   protected _templates: Record<string, string> = {
@@ -29,7 +84,11 @@ export default class LatexCharRenderer extends CharRenderer<string> {
     } else {
       template = findByClass(char, this._templates) || "";
     }
-    return replace(template, [char.value, ...params]);
+    let charValue = char.value;
+    if (char instanceof Variable) {
+      charValue = `{${greekLetters[charValue] || charValue}}`;
+    }
+    return replace(template, [charValue, ...params]);
   }
 
   protected _setParenthesesLevel(latex: string, level: number) {
--- src/models/renderer/mathjax-math-ml/CharRenderer.ts
+++ src/models/renderer/mathjax-math-ml/CharRenderer.ts
@@ -1,7 +1,7 @@
 import Config from "../../Config";
 import MathMLNode from "../../MathMLNode";
 import { findByClass } from "../../utils";
-import { Cursor, MathChar, Placeholder } from "../../math-char";
+import { Cursor, MathChar, Placeholder, Variable } from "../../math-char";
 import BaseRenderer from "../CharRenderer";
 
 type TemplateParam = {
@@ -17,6 +17,61 @@ const parenAttrs = [
   { stretchy: "true", minsize: "2.047em", maxsize: "2.047em" },
   { stretchy: "true", minsize: "2.470em", maxsize: "2.470em" },
 ];
+
+const greekLetters: Record<string, { value: string; mathvariant?: string }> = {
+  "lower-alpha": { value: "&#x3B1;" },
+  "upper-alpha": { value: "A" },
+  "lower-beta": { value: "&#x3B2;" },
+  "upper-beta": { value: "B" },
+  "lower-gamma": { value: "&#x3B3;" },
+  "upper-gamma": { value: "&#x393;", mathvariant: "normal" },
+  "lower-delta": { value: "&#x3B4;" },
+  "upper-delta": { value: "&#x394;", mathvariant: "normal" },
+  "lower-epsilon": { value: "&#x3F5;" },
+  "var-epsilon": { value: "&#x3B5;" },
+  "upper-epsilon": { value: "E" },
+  "lower-zeta": { value: "&#x3B6;" },
+  "upper-zeta": { value: "Z" },
+  "lower-eta": { value: "&#x3B7;" },
+  "upper-eta": { value: "H" },
+  "lower-theta": { value: "&#x3B8;" },
+  "var-theta": { value: "&#x3D1;" },
+  "upper-theta": { value: "&#x398;", mathvariant: "normal" },
+  "lower-iota": { value: "&#x3B9;" },
+  "upper-iota": { value: "I" },
+  "lower-kappa": { value: "&#x3BA;" },
+  "upper-kappa": { value: "K" },
+  "lower-lambda": { value: "&#x3BB;" },
+  "upper-lambda": { value: "&#x39B;", mathvariant: "normal" },
+  "lower-mu": { value: "&#x3BC;" },
+  "upper-mu": { value: "M" },
+  "lower-nu": { value: "&#x3BD;" },
+  "upper-nu": { value: "N" },
+  "lower-xi": { value: "&#x3BE;" },
+  "upper-xi": { value: "&#x39E;", mathvariant: "normal" },
+  "lower-o": { value: "o" },
+  "upper-o": { value: "O" },
+  "lower-pi": { value: "&#x3C0;" },
+  "upper-pi": { value: "&#x3A0;", mathvariant: "normal" },
+  "lower-rho": { value: "&#x3C1;" },
+  "var-rho": { value: "&#x3F1;" },
+  "upper-rho": { value: "P" },
+  "lower-sigma": { value: "&#x3C3;" },
+  "upper-sigma": { value: "&#x3A3;", mathvariant: "normal" },
+  "lower-tau": { value: "&#x3C4;" },
+  "upper-tau": { value: "T" },
+  "lower-upsilon": { value: "&#x3C5;" },
+  "upper-upsilon": { value: "&#x3A5;", mathvariant: "normal" },
+  "lower-phi": { value: "&#x3D5;" },
+  "var-phi": { value: "&#x3C6;" },
+  "upper-phi": { value: "&#x3A6;", mathvariant: "normal" },
+  "lower-chi": { value: "&#x3C7;" },
+  "upper-chi": { value: "X" },
+  "lower-psi": { value: "&#x3C8;" },
+  "upper-psi": { value: "&#x3A8;", mathvariant: "normal" },
+  "lower-omega": { value: "&#x3C9;" },
+  "upper-omega": { value: "&#x3A9;", mathvariant: "normal" },
+};
 
 export default class CharRenderer extends BaseRenderer<MathMLNode> {
   protected _templates: Record<string, TemplateParam> = {
@@ -61,7 +116,15 @@ export default class CharRenderer extends BaseRenderer<MathMLNode> {
     if (params.length > 0) {
       node.children = params;
     } else {
-      node.value = template.value || char.value;
+      if (char instanceof Variable && char.value in greekLetters) {
+        const greekLetter = greekLetters[char.value];
+        node.value = greekLetter.value;
+        if (greekLetter.mathvariant) {
+          node.setAttr({ mathvariant: greekLetter.mathvariant });
+        }
+      } else {
+        node.value = template.value || char.value;
+      }
     }
     return node;
   }
