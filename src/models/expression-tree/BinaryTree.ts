e0d8b4cd37dcd4bbed6686e65d84e06f8d208123
Yifan Dong <588153@qq.com>
2022-05-10 10:17:01 +0800

    feat: add parent node & relation to binary node traverse

--- src/models/expression-tree/BinaryTree.ts
+++ src/models/expression-tree/BinaryTree.ts
@@ -20,12 +20,47 @@ class BinaryNode<V, T extends BinaryTreeInterface>
     this.tree = tree;
   }
 
+  /**
+   * Traverse node by Left-Right-Node order.
+   *
+   * The first parameter is a function which will be run on the left child, the right child and the node, and
+   * it has 5 parameters to indicates the current node, the traverse result of the left child, the traverse
+   * result of the right child, the parent node of the current node (if exists), and the relation between the
+   * parent node and the current one (if exists).
+   *
+   * The relation should be "left" if the child node is the left child node of the parent node, and "right"
+   * vice versa.
+   *
+   * @param fn actual function on each node.
+   * @param parentNode parent node if exists. node should be root node of binary tree if `parentNode` is undefined.
+   * @param parentRelation "left" if the current node is the left child of the parent node, "right" vice versa.
+   * @returns the result of the first parameter function `fn`
+   */
   traverse<R>(
-    fn: (node: this, leftResult: R | undefined, rightResult: R | undefined) => R
+    fn: (
+      node: this,
+      leftResult: R | undefined,
+      rightResult: R | undefined,
+      parentNode?: this,
+      parentRelation?: "left" | "right"
+    ) => R,
+    parentNode?: this,
+    parentRelation?: "left" | "right"
   ): R {
-    const leftChildResult = this.leftChild?.traverse<R>(fn),
-      rightChildResult = this.rightChild?.traverse<R>(fn);
-    return fn(this, leftChildResult, rightChildResult);
+    /* use `this.leftChild?.travserse(...)` will cause `this` type change to `NonNullable<this>` */
+    const leftChildResult = this.leftChild
+        ? this.leftChild.traverse<R>(fn, this, "left")
+        : undefined,
+      rightChildResult = this.rightChild
+        ? this.rightChild.traverse<R>(fn, this, "right")
+        : undefined;
+    return fn(
+      this,
+      leftChildResult,
+      rightChildResult,
+      parentNode,
+      parentRelation
+    );
   }
 }
 
