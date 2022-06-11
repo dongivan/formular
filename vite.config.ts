f86e9f01501ce048c1d86e7f2a37aa5ef59bc07f
Yifan Dong <6535256+dongivan@users.noreply.github.com>
2022-06-11 16:10:04 +0800

    chore: add analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import * as path from "path";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import eslintPlugin from "vite-plugin-eslint";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,13 @@ export default defineConfig({
       customDomId: "__svg__icons__dom__",
     }),
     eslintPlugin(),
+    process.env.npm_lifecycle_event == "analyze"
+      ? visualizer({
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+        })
+      : undefined,
   ],
   resolve: {
     alias: {
